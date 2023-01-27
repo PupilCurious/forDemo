e185a207a21e5838980afce6643ed9d08f43a8ec
PupilCurious <ertugrul.bilal78@gmail.com>
2023-01-27 14:47:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+       for (int i = 1 ; i<10 ; i+=2){
+           System.out.println("i " + i);
+       }
+
+        //while
+        int j = 0;
+        while(j<10){
+            j+=2;
+            System.out.println("j " + j);
+        }
+
+        //do while
+        int k = 0;
+        do{
+            k+=3;
+            System.out.println("k " + k);
+        }while(k<10);
+
+
+
+    }
+}
